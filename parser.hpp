6935fd2b81b1db39d3728390ec1f2f9ae506d244
Babis Boloudakis <34814013+babisboloudakis@users.noreply.github.com>
2018-12-07 02:02:35 +0200

    parser

--- parser.hpp
+++ parser.hpp
@@ -3,17 +3,55 @@
 #include <iostream>
 #include <cstdint>
 #include <cstring>
- 
+
+typedef struct SelectInfo { 
+
+    // struct that contains info for select, relation.col
+    uint64_t rel;
+    uint64_t col;
+    // Struct constructor
+    SelectInfo(uint64_t rel, uint64_t col) : rel(rel), col(col) {};
+
+} SelectInfo;
+
+
+typedef struct FilterInfo {
+
+    enum FilterOperation : char {
+        // enumeration of all filter operations
+        LESS = '<',
+        GREATER = '>',
+        EQUAL = '='
+    };
+
+    // info about a filter operation
+    uint64_t rel;
+    uint64_t col;
+    uint64_t constant;
+    FilterOperation op;
+
+    // Constructor for filterInfo
+    FilterInfo(uint64_t rel, uint64_t col, uint64_t constant, FilterOperation op) : rel(rel), col(col), constant(constant), op(op) {};
+
+} FilterInfo;
+static const std::vector<FilterInfo::FilterOperation> comparisonTypes { FilterInfo::FilterOperation::LESS, FilterInfo::FilterOperation::GREATER, FilterInfo::FilterOperation::EQUAL};
+
+
 class Parser {
 
     public:
     // Vector of selections
-    std::vector<uint64_t> selections;
-
+    std::vector<uint64_t> relations;
+    std::vector<SelectInfo> selections;
     
+
     public:
     Parser(){};
     ~Parser(){};
+
+    // parse rel col pair < x.y >
+    void parseRelCol( std::string & rawPair );
+    void parsePredicate( std::string & predicate );
 
     void parseRelations( std::string & rawSelections );
     void parsePredicates( std::string & rawPredicates );
