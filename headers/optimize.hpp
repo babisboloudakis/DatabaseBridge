2e49fcd1f7f6b8069ea55020ba0dc002df4189b5
GeorgeNiarkhos <sdi1500109@di.uoa.gr>
2019-01-06 03:07:26 +0200

    progressing optimize query
    holiday work

--- headers/optimize.hpp
+++ headers/optimize.hpp
@@ -5,10 +5,38 @@
 
 #include "parser.hpp"
 
+
+typedef struct Stats {
+    int rel;
+    int col;
+    uint64_t min;
+    uint64_t max;
+    uint64_t size;
+    uint64_t distinct;
+    
+    // Struct constructor
+    Stats (int rel, int col, uint64_t min, 
+    uint64_t max, uint64_t size, uint64_t distinct) : 
+    rel(rel), col(col), min(min), max(max), size(size), distinct(distinct) {};
+
+} Stats;
+
+typedef struct relInfo {
+    int rel;
+    vector<Stats> * relStats;
+} relInfo;
+
+
+
 class Optimize{
     public:
 
-    Optimize(){};
+    int relcnt;
+    vector<relInfo> rels;
+
+    Optimize(int relcnt, FilterInfo & filter) : relcnt(relcnt){
+
+    };
     ~Optimize(){};
 
     void optimizeQuery(FileArray & fileArray, Parser & parser);
