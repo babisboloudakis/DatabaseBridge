61fd1f7c2e0fb896127e3de0cec2c7c96bf238b2
Babis Boloudakis <34814013+babisboloudakis@users.noreply.github.com>
2019-01-20 03:40:13 +0200

    clean harness

--- main.cpp
+++ main.cpp
@@ -9,7 +9,6 @@ int main(int argc, char* argv[]) {
 
     vector <string> fileNames;
     string line;
-    
     while (getline(cin, line)){
         if (line == "Done") break;
         fileNames.push_back(line);
